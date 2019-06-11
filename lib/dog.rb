01c56874b3607340278901260d74ca517a756550
John Jordan Shelley <jshelley553@gmail.com>
2019-06-11 05:43:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,10 @@
 class Dog
+  attr_accessor :breed, :name
 
-  def initialize(name)
+
+  def initialize(name, breed = "Mut")
+    @breed = breed
     @name = name
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
 end
-
-fido = Dog.new("Fido", "Pug")
-fido.name
